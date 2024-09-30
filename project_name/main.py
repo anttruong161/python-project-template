04d6f8445bef78dd94f7efda46c68e34b7ad7ec3
antt <anttruong161@gmail.com>
2024-09-30 09:50:22 +0700

    first commit

---
+++ project_name/main.py
@@ -0,0 +1,14 @@
+'''
+Example main module
+'''
+NAME = "project_name"
+
+def hello_world():
+    '''
+    Print Hello World
+    '''
+    print("Hello World")
+
+if __name__ == "__main__":
+    hello_world()
+    
---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+'''
+Example test main module
+'''
+from project_name.main import NAME
+
+def test_main():
+    '''
+    Test main logic flow
+    '''
+    assert NAME == "project_name"
+
+if __name__ == "__main__":
+    test_main()
+    
